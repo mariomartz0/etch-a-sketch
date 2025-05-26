d15eed0f0e8b338eea3045ffed46876573278e23
mariomartz0 <mariomartz2018@gmail.com>
2025-05-26 17:55:33 -0400

    Create functionality to change options
    Needed to create the ability to change the grid dimensions, the color
    the grid is painted, and a button to clear the grid.

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,38 @@
 // Create a webpage with a 16x16 grid of square divs
+let gridSize = 16;
+let tileColor = "black";
+createGrid(gridSize);
 
-createGrid(16);
+// Listener to draw on grid
+const grid = document.querySelector(".grid");
+grid.addEventListener("mouseover", (event) => {
+  let target = event.target;
+
+  if (target.classList[0] === "col") {
+    target.style.backgroundColor = tileColor;
+  }
+});
+
+// Input to change grid size
+const rangePicker = document.querySelector("#range_picker");
+rangePicker.addEventListener("change", () => {
+  gridSize = rangePicker.value;
+  createGrid(gridSize);
+});
+
+// Input to change color
+const colorPicker = document.querySelector("#color_picker");
+colorPicker.addEventListener("change", () => {
+  tileColor = colorPicker.value;
+});
+
+// Button to clear grid
+const clearBtn = document.querySelector("#clear_btn");
+clearBtn.addEventListener("click", () => createGrid(gridSize));
 
 function createGrid(gridSize) {
-  const container = document.querySelector(".container");
-
-  const grid = document.createElement("div");
-  grid.classList.add("grid");
-  container.appendChild(grid);
+  const grid = document.querySelector(".grid");
+  clearGrid(grid);
 
   // Create 16 rows
   for (let rowNum = 1; rowNum <= gridSize; rowNum++) {
@@ -17,8 +42,14 @@ function createGrid(gridSize) {
     // Create 16 columns in each row
     for (let colNum = 1; colNum <= gridSize; colNum++) {
       const col = document.createElement("div");
-      col.classList.add("col", `col-${colNum}`);
+      col.classList.add("col", `col-${colNum}`, `row-${rowNum}`);
       row.appendChild(col);
     }
+  }
+}
+
+function clearGrid(grid) {
+  while (grid.firstChild) {
+    grid.removeChild(grid.firstChild);
   }
 }
